042afd69d9629d5e4383cf8d3bfae583c3500710
saritacutinha <37643735+saritacutinha@users.noreply.github.com>
2020-04-15 23:40:43 +0930

    Dependency Injection Beginner Tutorial
    Dependency Injection Beginner Tutorial

---
+++ DependencyInjection/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DependencyInjection.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DependencyInjection.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IProductService _productService;
+
+        public HomeController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_productService.getAll());
+        }
+    }
+}
---
+++ DependencyInjection/Services/ProductService.cs
@@ -0,0 +1,29 @@
+﻿using DependencyInjection.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DependencyInjection.Services
+{
+
+    public interface IProductService
+    {
+        List<ProductViewModel> getAll();
+    }
+
+    public class ProductService : IProductService
+    {
+        public List<ProductViewModel> getAll()
+        {
+            return new List<ProductViewModel>
+            {
+                new ProductViewModel { Id = 1, Name = "Pen Drive" },
+                new ProductViewModel { Id = 2, Name = "Memory Card" },
+                new ProductViewModel { Id = 3, Name = "Mobile Phone" },
+                new ProductViewModel { Id = 4, Name = "Tablet" },
+                new ProductViewModel { Id = 5, Name = "Desktop PC" }
+            };
+        }
+    }
+}
