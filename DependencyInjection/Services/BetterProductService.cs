1af8ff2b29241728f2579f78e213c50fd5a53eb1
saritacutinha <37643735+saritacutinha@users.noreply.github.com>
2020-04-16 11:57:59 +0930

    Adding BetterProduct service class to demo loose Coupling

---
+++ DependencyInjection/Services/BetterProductService.cs
@@ -0,0 +1,18 @@
+﻿using DependencyInjection.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DependencyInjection.Services
+{
+    public class BetterProductService : IProductService
+    {
+        public List<ProductViewModel> getAll()
+        {
+            return( new List<ProductViewModel>
+            { new ProductViewModel {Id=1, Name="Telivision"},
+            new ProductViewModel {Id=2, Name="Telephone"} });
+        }
+    }
+}
--- DependencyInjection/Startup.cs
+++ DependencyInjection/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DependencyInjection.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -16,6 +17,8 @@ namespace DependencyInjection
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            services.AddTransient<IProductService, ProductService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,17 +27,32 @@ namespace DependencyInjection
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-            }
 
+
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseAuthentication();
             app.UseRouting();
+
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
+
+
+
     }
-}
+    }
+
